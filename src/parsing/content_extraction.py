5d6c72732d7f8c62beff1e36aa014adbbb5df750
parvathy <pmoha4@unh.newhaven.edu>
2017-11-04 22:23:21 -0400

    Deleting parsing folder, it is moved to package crawler

--- src/parsing/content_extraction.py
+++
@@ -1,9 +0,0 @@
-from newspaper import Article
-
-def get_text(url):
-    """Gets the content text from the url"""
-    article = Article(url)
-    article.download();
-    article.parse();
-    article.nlp();
-    return article.text;
