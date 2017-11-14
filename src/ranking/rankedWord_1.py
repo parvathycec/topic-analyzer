920779d95593c8d73d823591a853bd48cdce1a86
Saranya Balaji <saranyabalaji92@gmail.com>
2017-11-14 00:53:17 -0500

    ranking class

--- src/ranking/rankedWord_1.py
+++ src/ranking/rankedWord_1.py
@@ -1,6 +1,6 @@
 
 class RankedWord:
-    def __init__(self,word,url,score):
+    def __init__(self,word,score):
         self.score = score
         self.word = word
 
@@ -34,70 +34,17 @@ def check_all_cases_rank(key,content,total_rank):
         
 
 def do_rank(url,word):
-
-    req= requests.get(url)
-    soup = BeautifulSoup(req.text,"lxml")
-    
-    title_rank = 0
-    meta_rank = 0
-    url_rank = 0
-    occurences_rank = 0
-    no_of_occurences = 0
-    no_of_occurences_lower_case = 0
-    no_of_occurences_upper_case = 0
-    no_of_occurences_title_case = 0
-    no_of_occurences_mixed_case = 0
-    h1_tag_rank = 0
-
-    #get the title content
-    title_content = soup.title.string
-    
-    title_rank = check_all_cases_rank(word,title_content,5)
-    
-    meta_content = ""
-    for meta_tag in soup.findAll("meta"):
-       meta_con =  meta_tag.get("content",None)
-       if meta_con != None:
-           meta_content += meta_con + " "
-
-    meta_rank = check_all_cases_rank(word,meta_content,5)
-    
-    text = soup.get_text()
-    
-
-    no_of_occurences_lower_case = text.count(word.lower())
-    no_of_occurences_upper_case = text.count(word.upper())
-    no_of_occurences_title_case = text.count(word.title())
-
-    if word.isupper ==  False:
-        if word.islower == False:
-            if word.istitle == False:
-                no_of_occurences_mixed_case = text.count(word())
-    
-    no_of_occurences = no_of_occurences_lower_case + no_of_occurences_upper_case + no_of_occurences_title_case + no_of_occurences_mixed_case
-    
-    
-    occurences_rank = no_of_occurences
-
-    url_rank = check_all_cases_rank(word,url,5)
-    
-    
-
-    for h1_tag in soup.find_all("h1"):
-        h1_tag_content = h1_tag.string
-
-    h1_tag_rank = check_all_cases_rank(word,h1_tag_content,10)
-    
-    total_rank = title_rank + meta_rank + url_rank + occurences_rank + h1_tag_rank
-    
-    
+    score = 0
+    ranking = RankedWord(word,score)
 
     
-    return word,title_rank,meta_rank,url_rank,occurences_rank,h1_tag_rank,total_rank
+
+    ranking.score = 11
+
+    return ranking
 
 
-print(do_rank(url,"python"))
-
+\
 
         
     
