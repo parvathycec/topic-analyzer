b64704f6c59d9ef421b1dbd25875c2cc8e6e410f
Saranya Balaji <saranyabalaji92@gmail.com>
2017-11-17 01:42:28 -0500

    modifications to the ranking

--- src/ranking/ranking_v2.py
+++ src/ranking/ranking_v2.py
@@ -4,23 +4,34 @@ import RankedWord  #import the class file
 from newspaper import Article
 from newspaper.article import ArticleException
 
-#from __init__ import META_SCORE ,OCCURENCE_SCORE, POS_SCORE ,URL_SCORE, TITLE_SCORE ,H1_SCORE 
 
+from __init__ import META_SCORE , NO_OF_TOKENS ,OCCURENCE_SCORE, POS_SCORE ,URL_SCORE, TITLE_SCORE ,H1_SCORE 
 
-def check_all_cases_rank(key,content,total_rank):
+def calculate_rank(key,content,total_rank,freq = 0):
 
-    #this function checks for the key in the respective content across all the cases and assigns the rank
-    rank = 0
-    if key in content:
-        rank = total_rank
-    elif key.lower() in content:
-        rank = total_rank
-    elif key.upper() in content:
-        rank = total_rank
-    elif key.title() in content:
-        rank = total_rank
+    if freq != 0:
+        rank =0
+        content = content.split(" ")
+        content = [x.lower() for x in content]
+        indices = []
+        if " " in key: #if it is a phrase
+            phrases = key.split(" ")
+            for p in phrases:
+                if p.lower() in content:
+                    indices.append(content.index(p.lower()))
+                else:
+                    return 0
+        else:
+            if key in content:
+                rank = total_rank
+                return rank
 
-    return rank
+    else: #no of occurences
+
+        rank
+        
+                
+
         
 def calculate_url_score(url,word):
     #checks whether the word/phrase appears in the url,if it appears, then calculate a score accordingly
@@ -30,15 +41,26 @@ def calculate_url_score(url,word):
     print(check_all_cases_rank(word,url,URL_SCORE))
     return check_all_cases_rank(word,url,URL_SCORE)
 
+def calculate_nof_token(word):
+    if " " in word:
+        nof_token_score = NO_OF_TOKENS
+
+    else:
+        nof_token_score = 0
+
+    return nof_token_score
+
+    return nof_token_score
+
 def calculate_title_score(soup,word):
     
     title_content = soup.title.string
-    print("title")
-    print(check_all_cases_rank(word,title_content,TITLE_SCORE))
-    return check_all_cases_rank(word,title_content,TITLE_SCORE)
 
+    return calculate_rank(word,title_content,TITLE_SCORE)
+
+    
 def calculate_meta_score(soup,word):
-    meta_content = ""
+    meta_content = None
     for meta_tag in soup.findAll("meta"):
        meta_con =  meta_tag.get("content",None)
        if meta_con != None:
@@ -47,9 +69,9 @@ def calculate_meta_score(soup,word):
     print("meta")      
     #check whether the word occurs in the meta data and assign rank
     if meta_content != None:
-        print(check_all_cases_rank(word,meta_content,META_SCORE))
-        return check_all_cases_rank(word,meta_content,META_SCORE)
 
+        return calculate_rank(word,meta_content,META_SCORE)
+        
     else:
         return 0
 
@@ -63,11 +85,10 @@ def calculate_h1_score(soup,word):
 
     #if the word appears in the h1, then assign rank accordingly
     if h1_tag_content != None:
-        h1_tag_rank = check_all_cases_rank(word,h1_tag_content,H1_SCORE)
+        h1_tag_rank = calculate_rank(word,h1_tag_content,H1_SCORE)
     else:
         h1_tag_rank = 0
-    print("h1")
-    print(h1_tag_rank)
+        
     return h1_tag_rank
         
 def calculate_occurances_score(word,url):
@@ -82,6 +103,9 @@ def calculate_occurances_score(word,url):
         #article.nlp();
         article_content = article.title
 
+    if " " in word:
+        
+        
     if " " in word: #if there is a space in between the word, then it is considered as a phrase
         score = article_content.count(word)
         
@@ -91,6 +115,7 @@ def calculate_occurances_score(word,url):
     else:
 
         article_content_words = article_content.split(" ")
+        article_content_words
 
         score_1 = article_content_words.count(word)
         score_2 = article_content_words.count(word.lower())
@@ -133,11 +158,19 @@ def do_rank(url,rank_obj):
 
     occurances_score = calculate_occurances_score(word,url)
     
-    if rank_obj.isPos == "True":
+    no_of_token_score = 0
+    no_of_token_score = calculate_nof_token(word)
+    
+    if rank_obj.isPos == True:
         pos_score = POS_SCORE
-        
+
+    additional_meta_score = 0
+
+    if pos_score!= 0 and title_score != 0:
+        additional_meta_score = 50
     #total rank
-    total_rank = url_score + title_score + meta_score + h1_score + occurances_score + pos_score
+    
+    total_rank = url_score + additional_meta_score + no_of_token_score +title_score + meta_score + h1_score + occurances_score + pos_score
     
     rank_obj.score = total_rank
 
