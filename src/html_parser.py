740b63b710f7de6ced3be2c0664fe58ff736997c
Saranya Balaji <saranyabalaji92@gmail.com>
2017-11-21 14:25:53 -0500

    modifications to html parser

--- src/html_parser.py
+++ src/html_parser.py
@@ -1,7 +1,7 @@
 import urllib.request
-
+import requests
 from bs4 import BeautifulSoup
-website_url = "http://news.bbc.co.uk/2/hi/health/2284783.stm"
+
 def htmlParser(website_url):
     
     url = urllib.request.urlopen(website_url)
@@ -16,3 +16,21 @@ def htmlParser(website_url):
     text = ''.join(chunk for chunk in chunks if chunk)
     return text
      
+
+def parse_html(url):
+    req= requests.get(url)  #get the url request
+    soup = BeautifulSoup(req.text,"lxml") #parse it through BeautifulSoup
+    #1. get title
+    title_content = soup.title.string
+    #2. get metatags
+    meta_content = ""
+    for meta_tag in soup.findAll("meta"):
+       meta_con =  meta_tag.get("content",None)
+       if meta_con != None:
+           meta_content += meta_con + " "
+    #3. Get h1 tags
+    h1_tag_content = None
+    for h1_tag in soup.find_all("h1"):
+        h1_tag_content = h1_tag.string
+    return title_content, meta_content, h1_tag_content;       
+    
